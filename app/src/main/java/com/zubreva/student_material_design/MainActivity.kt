15e5a85cbcc62acae28cf32107d68beccb6eceaa
duxa4854 <nzaichenko531@gmail.com>
2026-03-04 15:02:41 +0300

    Step 5: Basic UI completed

--- app/src/main/java/com/zubreva/student_material_design/MainActivity.kt
+++ app/src/main/java/com/zubreva/student_material_design/MainActivity.kt
@@ -1,16 +1,31 @@
 package com.zubreva.student_material_design
 
 import android.os.Bundle
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.ui.res.stringResource
+import androidx.annotation.StringRes
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.Image
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Surface
+import com.zubreva.student_material_design.data.Student
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.zubreva.student_material_design.data.students
 import com.zubreva.student_material_design.ui.theme.Student_Material_DesignTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,8 +34,69 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Student_Material_DesignTheme {
-
+                Surface(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    StudentApp()
+                }
             }
         }
+    }
+}
+@Composable
+fun StudentIcon(
+    @DrawableRes studentIcon: Int,
+    modifier: Modifier = Modifier
+) {
+    Image(
+        modifier = modifier
+            .size(dimensionResource(R.dimen.image_size))
+            .padding(dimensionResource(R.dimen.padding_small)),
+        painter = painterResource(studentIcon),
+        contentDescription = null
+    )
+}
+@Composable
+fun studentInformation(
+    @StringRes studentName: Int,
+    studentAge: Int,
+    modifier: Modifier = Modifier
+
+) {
+    Column(modifier = modifier) {
+        Text(
+            text = stringResource(studentName),
+            modifier = Modifier.padding(top = dimensionResource(R.dimen.padding_small)),
+            style = MaterialTheme.typography.displayMedium
+        )
+    }
+}
+@Composable
+fun StudentItem(
+    student: Student,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(dimensionResource(R.dimen.padding_small))
+    ) {
+        StudentIcon(student.imageResourceId)
+        studentInformation(student.name, student.age)
+    }
+}
+@Composable
+fun StudentApp() {
+    LazyColumn {
+        items(students) {
+            StudentItem(student = it)
+        }
+    }
+}
+@Preview
+@Composable
+fun StudentPreview() {
+    Student_Material_DesignTheme(darkTheme = false) {
+        StudentApp()
     }
 }
