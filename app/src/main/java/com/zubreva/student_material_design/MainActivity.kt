be3a314efaa8472f1c7e5c1efb511f321cdb699f
duxa4854 <nzaichenko531@gmail.com>
2026-03-10 13:03:40 +0300

    Step 10: Top App Bar added

--- app/src/main/java/com/zubreva/student_material_design/MainActivity.kt
+++ app/src/main/java/com/zubreva/student_material_design/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.Text
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.res.stringResource
@@ -22,12 +23,16 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Card
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import com.zubreva.student_material_design.data.Student
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.tooling.preview.Preview
 import com.zubreva.student_material_design.data.students
 import com.zubreva.student_material_design.ui.theme.Student_Material_DesignTheme
@@ -99,10 +104,15 @@ fun StudentItem(
 }
 @Composable
 fun StudentApp() {
-    Scaffold { it ->
+    Scaffold(
+        topBar = {
+            StudentTopAppBar()
+        }
+    ) { it ->
         LazyColumn(contentPadding = it) {
             items(students) {
-                StudentItem(student = it,
+                StudentItem(
+                    student = it,
                     modifier = Modifier
                         .padding(dimensionResource(R.dimen.padding_small))
                 )
@@ -123,4 +133,29 @@ fun StudentDarkThemePreview() {
     Student_Material_DesignTheme(darkTheme = true) {
         StudentApp()
     }
+}
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+fun StudentTopAppBar(modifier: Modifier = Modifier) {
+    CenterAlignedTopAppBar(
+        title = {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+
+            ) {
+                Image(
+                    modifier = Modifier
+                        .size(dimensionResource(R.dimen.image_size))
+                        .padding(dimensionResource(R.dimen.padding_small)),
+                    painter = painterResource(R.drawable.ic_student_logo),
+                    contentDescription = null
+                )
+                Text(
+                    text = stringResource(R.string.app_name),
+                    style = MaterialTheme.typography.displayLarge
+                )
+            }
+        },
+        modifier = modifier
+    )
 }
